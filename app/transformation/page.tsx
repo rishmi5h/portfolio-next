5f9ca62e1980eb0128e918260b7bb92fb26e5adf
RISHABH MISHRA <rish.mish.2607@gmail.com>
2023-08-15 00:36:58 +0530

    add : transformation route 🏋🏼‍♂️

---
+++ app/transformation/page.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+
+export const metadata = {
+  title: "rishmi5h | transformation",
+  openGraph: {
+    title: "Transformation",
+  },
+};
+
+export default function Transformation() {
+  return (
+    <div className='flex flex-col items-center justify-between p-24'>
+      <h1>This is Transformation page</h1>
+    </div>
+  );
+}

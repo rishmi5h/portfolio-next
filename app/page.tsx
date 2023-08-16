1e89bf2c5b1c3deed8195adafb5096a3f387402e
RISHABH MISHRA <rish.mish.2607@gmail.com>
2023-08-17 01:59:40 +0530

    update : home page 🚧

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,19 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className='flex flex-col items-center justify-between p-24'>
-      <h1>Home Page</h1>
-    </main>
+    <div>
+      <div className='flex flex-col justify-between p-6 w-2/5 ml-20 my-20'>
+        <p className='text-gray-500 text-3xl'>SOFTWARE ENGINEER</p>
+        <div>
+          <h1 className='text-5xl py-5 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 bg-clip-text text-transparent'>Rishabh Mishra</h1>
+        </div>
+        <div className='border-l-2 pl-4 text-gray-500 mt-5'>
+          <p className='w-1/2 text-lg'>
+            I am a full-stack developer working from India.
+          </p>
+        </div>
+      </div>
+      <div></div>
+    </div>
   );
 }
